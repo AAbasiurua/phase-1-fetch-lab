d7cb3397de9543b725cd0791abd1b2f3d65a3fd0
AniekanabasiAbasiurua <aniekanabasi.abasiurua@gmail.com>
2025-01-04 23:22:08 -0500

    phase-1-fetch-lab-complete

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      renderBooks(data);
+      console.log('slam')
+    });
+}      
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
